734bcecc6a48961441289410a4139d1186ae566c
Adam Balsam <balsam.adam@gmail.com>
2021-11-10 22:59:39 -0500

    Allow configurable usernamr

--- content.js
+++ content.js
@@ -1,29 +1,33 @@
-var elements = document.getElementsByClassName('comment');
-var userNameToHide = 'MisterMagooForYou';
-var magooCommentsHidden = 0;
-for (var i = 0; i < elements.length; i++) {
-    var element = elements[i];
+chrome.storage.sync.get(['usernames'], function(result) {
+    var elements = document.getElementsByClassName('comment');
+    var userNameToHide = result.usernames;
 
-    var usernameElement = element.getElementsByClassName('username');
-    if (usernameElement) {
-        var username = usernameElement[0].innerHTML;
-        if (username === userNameToHide) {
-            var next = element.nextElementSibling;
+    var magooCommentsHidden = 0;
+    for (var i = 0; i < elements.length; i++) {
+        var element = elements[i];
 
-            if (next) {
-                // If there is another comment, AND it's indented, it's a reply! Remove that too.
-                var indented = next.getElementsByClassName('indented')
-                if (indented) {
-                    next.parentNode.removeChild(next);
+        var usernameElement = element.getElementsByClassName('username');
+        if (usernameElement) {
+            var username = usernameElement[0].innerHTML;
+            if (username === userNameToHide) {
+                var next = element.nextElementSibling;
+
+                if (next) {
+                    // If there is another comment, AND it's indented, it's a reply! Remove that too.
+                    var indented = next.getElementsByClassName('indented')
+                    if (indented) {
+                        next.parentNode.removeChild(next);
+                    }
                 }
-            }
 
-            element.parentNode.removeChild(element);
-            magooCommentsHidden++;
+                element.parentNode.removeChild(element);
+                magooCommentsHidden++;
+            }
         }
     }
-}
-if (magooCommentsHidden) {
-    const plural = (count, noun, suffix = 's') => `${count} ${noun}${count !== 1 ? suffix : ''}`;
-    console.log('Removed ' + plural(magooCommentsHidden, 'comment thread') + ' by magoo.');
-}
+    if (magooCommentsHidden) {
+        const plural = (count, noun, suffix = 's') => `${count} ${noun}${count !== 1 ? suffix : ''}`;
+        console.log('Removed ' + plural(magooCommentsHidden, 'comment thread') + ' by ' + userNameToHide + '.');
+    }
+
+});
